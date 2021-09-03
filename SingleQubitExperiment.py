94422e6f2ff4ac6818b5e6b4d37efd994bdd6f0c
Alexander Simm <alexander.simm@posteo.de>
2021-09-03 09:56:44 +0200

    moved a lot of code to the SingleQubitExperiment class

--- SingleQubitExperiment.py
+++ SingleQubitExperiment.py
@@ -1,5 +1,5 @@
 import pathlib
-from typing import Callable
+from typing import Callable, Dict, List
 import numpy as np
 import utils as utils
 import tensorflow as tf
@@ -15,14 +15,19 @@ from c3.signal.gates import Instruction
 
 class SingleQubitExperiment:
     __directory: str
+    __file_prefix: str
     __file_suffix: str
     __qubit: chip.Qubit
     __experiment: Experiment
     __gate: Instruction
     __t_final: float
     __init_state: tf.Tensor
+    __sequence: List[str]
 
-    def __init__(self, file_suffix: str, directory: str = None):
+    def __init__(
+        self, file_prefix: str = None, file_suffix: str = None, directory: str = None
+    ):
+        self.__file_prefix = file_prefix
         self.__file_suffix = file_suffix
 
         if directory is None:
@@ -46,29 +51,16 @@ class SingleQubitExperiment:
         self.__qubit = utils.createQubit(1, 5, freq, -anharm)
         model = utils.createModel([self.__qubit])
         generator = utils.createGenerator(model)
-        energies = self.__qubit.get_Hamiltonian().numpy().diagonal().real
-        print(
-            "energies: ",
-            [
-                (energies[i + 1] - energies[i]) / (2 * np.pi)
-                for i in range(len(energies) - 1)
-            ],
-        )
 
         # gate
-        # envelope = createPWCGaussianPulse(t_final, t_final / 4, 30)
-        ideal = qt_utils.np_kron_n(
-            [
-                constants.Id,
-                constants.x90p,
-            ]
-        )
+        ideal = qt_utils.np_kron_n([constants.Id, constants.x90p])
         self.__gate = utils.createSingleQubitGate(
             "lower-X", t_final, carrier_freq, envelope, model, self.__qubit, ideal
         )
-        gates = [self.__gate]
+        self.__sequence = [self.__gate.get_key()]
 
         # experiment
+        gates = [self.__gate]
         gate_names = list(map(lambda g: g.get_key(), gates))
         self.__experiment = Experiment(
             pmap=ParameterMap(instructions=gates, model=model, generator=generator)
@@ -78,32 +70,42 @@ class SingleQubitExperiment:
 
         # initial state
         self.__init_state = utils.createState(model, occupied_levels)
-        # state = init_state.numpy().flatten()
-        # print("initial state=", state, ", occupation=", exp.populations(state, model.lindbladian).numpy())
 
-        # time evolution and signal before optimisation
-        sequence = ["lower-X[0]"]
-        signal = generator.generate_signals(self.__gate)[
-            utils.getDrive(model, self.__qubit).name
+    def getEnergies(self):
+        return self.__qubit.get_Hamiltonian().numpy().diagonal().real
+
+    def generateSignal(self) -> Dict[str, tf.Tensor]:
+        """
+        Makes the generator generate a signal and returns it.
+        """
+        return self.__experiment.pmap.generator.generate_signals(self.__gate)[
+            utils.getDrive(self.__experiment.pmap.model, self.__qubit).name
         ]
+
+    def plotSignal(self, name: str) -> None:
+        """
+        Generates a signal, plots it, and saves it to a file with the given name in the experiment's directory.
+        """
+        signal = self.generateSignal()
         utils.plotSignal(
             signal["ts"].numpy(),
             signal["values"].numpy(),
-            self.__directory + f"/signal_before_{self.__file_suffix}.png",
+            self.__createFileName(name),
             spectrum_cut=1e-4,
         )
-        peakFrequencies, peakValues = utils.findFrequencyPeaks(
-            signal["ts"].numpy(), signal["values"].numpy(), 4
+
+    def runTimeEvolution(self) -> np.array:
+        return utils.runTimeEvolutionDefault(
+            self.__experiment, self.__init_state, self.__sequence
         )
-        print("peaks: ", np.sort(peakFrequencies))
-        populations = utils.runTimeEvolutionDefault(
-            self.__experiment, self.__init_state, sequence
-        )
+
+    def plotTimeEvolution(self, name: str) -> None:
+        populations = self.runTimeEvolution()
         utils.plotOccupations(
             self.__experiment,
             populations,
-            sequence,
-            filename=self.__directory + f"/populations_before_{self.__file_suffix}.png",
+            self.__sequence,
+            filename=self.__createFileName(name),
             level_names=[
                 "$|0,0\\rangle$",
                 "$|0,1\\rangle$",
@@ -139,45 +141,19 @@ class SingleQubitExperiment:
             # callback=callback,
             algorithm=algorithm,
             algo_options=algorithm_options,
-            log_dir=(
-                self.__directory
-                + ("/log_{0}_{1:.2f}/".format(self.__file_suffix, self.__t_final * 1e9))
-            ),
+            log_dir=self.__directory
+            + ("/log_{0}_{1:.2f}/".format(self.__file_suffix, self.__t_final * 1e9)),
         )
         print("before:\n", params_before)
         print("after:\n", params_after)
         print("fidelity:\n", final_fidelity)
-
-        # time evolution and signal after optimisation
-        sequence = [self.__gate.get_key()]
-        pmap = self.__experiment.pmap
-        signal = pmap.generator.generate_signals(self.__gate)[
-            utils.getDrive(pmap.model, self.__qubit).name
-        ]
-        utils.plotSignal(
-            signal["ts"].numpy(),
-            signal["values"].numpy(),
-            self.__directory + f"/signal_after_{self.__file_suffix}.png",
-            spectrum_cut=1e-4,
-        )
-        peakFrequencies, peakValues = utils.findFrequencyPeaks(
-            signal["ts"].numpy(), signal["values"].numpy(), 4
-        )
-        print("peaks: ", np.sort(peakFrequencies))
-        populations = utils.runTimeEvolutionDefault(
-            self.__experiment, self.__init_state, sequence
-        )
-        utils.plotOccupations(
-            self.__experiment,
-            populations,
-            sequence,
-            filename=self.__directory + f"/populations_after_{self.__file_suffix}.png",
-            level_names=[
-                "$|0,0\\rangle$",
-                "$|0,1\\rangle$",
-                "$|1,0\\rangle$",
-                "$|1,1\\rangle$",
-                "leakage",
-            ],
-        )
         return params_after
+
+    def __createFileName(self, name):
+        s = self.__directory + "/"
+        if self.__file_prefix is not None and len(self.__file_prefix) > 0:
+            s += self.__file_prefix + "_"
+        s += name
+        if self.__file_suffix is not None and len(self.__file_suffix) > 0:
+            s += self.__file_suffix + "_"
+        return s + ".png"
