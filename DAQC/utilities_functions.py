f7e6452fe107ec66d63d7f38ae0f7b40ee964fa9
Ashutosh-Mishra2 <ashutoshsitu99@gmail.com>
2021-10-27 12:08:06 +0200

    Utilities function

--- DAQC/utilities_functions.py
+++ DAQC/utilities_functions.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np
 import copy 
 import matplotlib.pyplot as plt
@@ -24,7 +25,7 @@ import c3.libraries.fidelities as fidelities
 import c3.libraries.envelopes as envelopes
 import c3.utils.qt_utils as qt_utils
 import c3.utils.tf_utils as tf_utils
-
+#%%
 
 def intialize_qubits(Num_qubits, qubit_levels_list, freq_list, anharm_list, t1_list, t2star_list, phi_list, phi0_list, d_list, qubit_temp):
     qubit_array = []
@@ -89,8 +90,8 @@ def intialize_qubits(Num_qubits, qubit_levels_list, freq_list, anharm_list, t1_l
     return qubit_array
 
 def initialize_coupler(Num_coupler, coupler_levels_list, freq_list, anharm_list, t1_list, t2star_list, phi_list, phi0_list, d_list, qubit_temp):
+    coupler_array = []
     for i in range(Num_coupler):
-        coupler_array = []
         coupler_array.append( 
                 chip.Transmon(
                         name = "C" + str(i+1),
@@ -149,31 +150,51 @@ def initialize_coupler(Num_coupler, coupler_levels_list, freq_list, anharm_list,
         )
 
 def generate_couplings(Num_qubits, Num_coupler, NN_coupling_strength, NNN_coupling_strength, qubit_array, coupler_array):
-    
-    ## Change the two loops to one loop
-    
+
+    ## For chain architecture of qubits and couplers
+
     g_NN_array = []
     g_NNN_array = []
-    for i in range(Num_coupler):
-        for j in range(Num_qubits):
-            if j == i + 1 or j == i: 
-                g_NN_array.append(
-                    chip.Coupling(
-                        name=qubit_array[j].name + "-" + coupler_array[i].name,
-                        desc="Coupling",
-                        comment="Coupling between "+ qubit_array[j].name + " and " + coupler_array[i].name,
-                        connected=[qubit_array[j].name, coupler_array[i].name],
-                        strength=Qty(
-                                value=NN_coupling_strength[i],
-                                min_val=-1*1e3,
-                                max_val=200e6,
-                                unit="Hz 2pi"
-                        ),
-                        hamiltonian_func=hamiltonians.int_XX
-                    )
-                )
-        else:
-            continue
+    
+    for i in range(Num_coupler + Num_qubits - 1):
+        coupler_index = int(np.floor(i/2) + 1)
+        qubit_index = int(np.floor((i+1)/2)+1) 
+        g_NN_array.append(
+            chip.Coupling(
+                name=qubit_array[qubit_index].name + "-" + coupler_array[coupler_index].name,
+                desc="Coupling",
+                comment="Coupling between "+ qubit_array[qubit_index].name + " and " + coupler_array[coupler_index].name,
+                connected=[qubit_array[qubit_index].name, coupler_array[coupler_index].name],
+                strength=Qty(
+                        value=NN_coupling_strength[i],
+                        min_val=-1*1e3,
+                        max_val=200e6,
+                        unit="Hz 2pi"
+                ),
+                hamiltonian_func=hamiltonians.int_XX
+            )
+        )
+
+    for i in range(len(NNN_coupling_strength)):
+        g_NNN_array.append(
+            chip.Coupling(
+                name=qubit_array[i].name + "-" + qubit_array[i+1].name,
+                desc="Coupling",
+                comment="Coupling between "+ qubit_array[i].name + " and " + qubit_array[i+1].name,
+                connected=[qubit_array[i].name, qubit_array[i+1].name],
+                strength=Qty(
+                        value=NNN_coupling_strength[i],
+                        min_val=-1*1e3,
+                        max_val=200e6,
+                        unit="Hz 2pi"
+                ),
+                hamiltonian_func=hamiltonians.int_XX
+            )
+        )
+
+    return g_NN_array + g_NNN_array
+
+    
 
         
 
