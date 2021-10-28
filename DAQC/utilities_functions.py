bd38545265eb83a5ba9b58a31d728ab143e90867
Ashutosh-Mishra2 <ashutoshsitu99@gmail.com>
2021-10-28 15:09:02 +0200

    Changed drive name

--- DAQC/utilities_functions.py
+++ DAQC/utilities_functions.py
@@ -292,8 +292,8 @@ def createDrives(qubits: List[chip.PhysicalComponent]) -> List[chip.Drive]:
     for i in range(len(qubits)):
         drives.append(
             chip.Drive(
-                name=f"d{i}",
-                desc=f"Drive {i}",
+                name=f"d{i+1}",
+                desc=f"Drive {i+1}",
                 comment=f"Drive line on qubit {qubits[i].name}",
                 connected=[qubits[i].name],
                 hamiltonian_func=hamiltonians.x_drive,
