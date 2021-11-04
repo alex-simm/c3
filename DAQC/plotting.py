53c06d78b6883f85680a8d315a7b7bd065d183cf
Alexander Simm <alexander.simm@posteo.de>
2021-11-04 11:18:37 +0100

    update plotting functions

--- DAQC/plotting.py
+++ DAQC/plotting.py
@@ -3,23 +3,67 @@ import numpy as np
 from matplotlib import pyplot as plt, colors, cm, figure
 import plotly.graph_objects as go
 
+from DAQC.utilities_functions import getQubitsPopulation
+from c3.experiment import Experiment
+import tensorflow as tf
 
-def plotSignal(time, signal, filename=None, spectrum_threshold=1e-4) -> figure.Figure:
+
+def plotSignal(time, signal, filename=None) -> figure.Figure:
     """
-    Plots a time dependent drive signal and its normalised frequency spectrum.
+    Plots a time dependent drive signal.
 
     Parameters
     ----------
     time
         timestamps
     signal
-        signal value
+        the function values
     filename: str
         Optional name of the file to which the plot will be saved. If none,
         it will only be shown.
-    spectrum_threshold:
+
+    Returns
+    -------
+    figure.Figure The figure in which the plot was created.
+    """
+    time = time.flatten()
+    signal = signal.flatten()
+    fig, ax = plt.subplots(1, 1, figsize=(12, 5))
+    ax.plot(time, signal)
+    ax.set_xlabel("Time")
+    ax.set_xlabel("Signal")
+
+    # show and save
+    plt.tight_layout()
+    if filename:
+        plt.savefig(filename, bbox_inches="tight", dpi=100)
+    plt.show()
+    plt.close()
+
+    return fig
+
+
+def plotSignalSpectrum(
+    time: np.array,
+    signal: np.array,
+    spectrum_threshold: float = 1e-4,
+    filename: str = None,
+) -> figure.Figure:
+    """
+    Plots the normalised frequency spectrum of a time-dependent signal.
+
+    Parameters
+    ----------
+    time: np.array
+        timestamps
+    signal: np.array
+        signal value
+    spectrum_threshold: float
         If not None, only the part of the normalised spectrum whose absolute square
         is larger than this value will be plotted.
+    filename: str
+        Optional name of the file to which the plot will be saved. If none,
+        it will only be shown.
 
     Returns
     -------
@@ -28,10 +72,7 @@ def plotSignal(time, signal, filename=None, spectrum_threshold=1e-4) -> figure.F
     # plot time domain
     time = time.flatten()
     signal = signal.flatten()
-    fig, axs = plt.subplots(1, 2, figsize=(12, 5))
-    axs[0].set_title("Signal")
-    axs[0].plot(time, signal)
-    axs[0].set_xlabel("Time")
+    fig = plt.figure()
 
     # calculate frequency spectrum
     freq_signal = np.fft.rfft(signal)
@@ -48,20 +89,17 @@ def plotSignal(time, signal, filename=None, spectrum_threshold=1e-4) -> figure.F
         normalised = normalised[limits[0] : limits[-1]]
 
     # plot frequency domain
-    axs[1].set_title("Spectrum")
-    axs[1].plot(freq, normalised.real, label="Re")
-    axs[1].plot(freq, normalised.imag, label="Im")
-    axs[1].plot(freq, np.abs(normalised) ** 2, label="Square")
-    axs[1].set_xlabel("frequency")
-    axs[1].legend()
+    plt.plot(freq, normalised.real, label="Re")
+    plt.plot(freq, normalised.imag, label="Im")
+    plt.plot(freq, np.abs(normalised) ** 2, label="Square")
+    plt.xlabel("frequency")
+    plt.legend()
 
     # show and save
     plt.tight_layout()
     if filename:
-        print("saving plot in " + filename)
         plt.savefig(filename, bbox_inches="tight", dpi=100)
-    else:
-        plt.show()
+    plt.show()
     plt.close()
 
     return fig
@@ -69,9 +107,10 @@ def plotSignal(time, signal, filename=None, spectrum_threshold=1e-4) -> figure.F
 
 def plotComplexMatrix(
     M: np.array,
-    colourMap: colors.Colormap,
+    colourMap: str = "nipy_spectral",
     xlabels: List[str] = None,
     ylabels: List[str] = None,
+    filename: str = None,
 ) -> figure.Figure:
     """
     Plots a complex matrix as a 3d bar plot, where the radius is the bar height and the phase defines
@@ -81,12 +120,15 @@ def plotComplexMatrix(
     ----------
     M : np.array
       the matrix to plot
-    colourMap : matplotlib.colors.Colormap
-      a Colormap to be used for the phases
+    colourMap : str
+      a name of a colormap to be used for the phases
     xlabels : List[str]
       labels for the x-axis
     ylabels : List[str]
       labels for the y-axis
+    filename: str
+        Optional name of the file to which the plot will be saved. If none,
+        it will only be shown.
 
     Returns
     -------
@@ -115,8 +157,9 @@ def plotComplexMatrix(
     # plot the bars
     fig = plt.figure()
     axis = fig.add_subplot(111, projection="3d")
+    colours = cm.get_cmap(colourMap)
     for idx, cur_zpos in enumerate(zpos):
-        color = colourMap((dz2[idx] + np.pi) / (2 * np.pi))
+        color = colours((dz2[idx] + np.pi) / (2 * np.pi))
         axis.bar3d(
             xpos[idx],
             ypos[idx],
@@ -132,16 +175,17 @@ def plotComplexMatrix(
     axis.view_init(elev=30, azim=-15)
     axis.set_xticks(np.arange(0.5, lx + 0.5, 1))
     axis.set_yticks(np.arange(0.5, ly + 0.5, 1))
-    tickFontSize = 13 - 2 * (len(xlabels) / 8)
     if xlabels is not None:
-        axis.w_xaxis.set_ticklabels(xlabels, fontsize=tickFontSize)
+        axis.w_xaxis.set_ticklabels(xlabels, fontsize=13 - 2 * (len(xlabels) / 8))
     if ylabels is not None:
-        axis.w_yaxis.set_ticklabels(ylabels, fontsize=tickFontSize, rotation=-65)
+        axis.w_yaxis.set_ticklabels(
+            ylabels, fontsize=13 - 2 * (len(ylabels) / 8), rotation=-65
+        )
 
     # colour bar
     norm = colors.Normalize(vmin=-np.pi, vmax=np.pi)
     cbar = fig.colorbar(
-        cm.ScalarMappable(norm=norm, cmap=colourMap),
+        cm.ScalarMappable(norm=norm, cmap=colours),
         ax=axis,
         shrink=0.6,
         pad=0.1,
@@ -149,15 +193,23 @@ def plotComplexMatrix(
     )
     cbar.ax.set_yticklabels(["$-\\pi$", "$\\pi/2$", "0", "$\\pi/2$", "$\\pi$"])
 
+    # show and save
+    plt.tight_layout()
+    if filename:
+        plt.savefig(filename, bbox_inches="tight", dpi=100)
+    plt.show()
+    plt.close()
+
     return fig
 
 
 def plotComplexMatrixAbsOrPhase(
     M: np.array,
-    colourMap: colors.Colormap,
+    colourMap: str = "nipy_spectral",
     xlabels: List[str] = None,
     ylabels: List[str] = None,
     phase: bool = True,
+    filename: str = None,
 ):
     """
     Plots the phase or absolute value of a complex matrix as a 2d colour plot.
@@ -166,14 +218,17 @@ def plotComplexMatrixAbsOrPhase(
     ----------
     M : np.array
       the matrix to plot
-    colourMap : matplotlib.colors.Colormap
-      a Colormap to be used for the phases
+    colourMap : str
+      name of a colour map to be used for the phases
     xlabels : List[str]
       labels for the x-axis
     ylabels : List[str]
       labels for the y-axis
     phase : bool
       whether the phase or the absolute value should be plotted
+    filename: str
+        Optional name of the file to which the plot will be saved. If none,
+        it will only be shown.
 
     Returns
     -------
@@ -191,9 +246,10 @@ def plotComplexMatrixAbsOrPhase(
     fig = plt.figure()
     axis = fig.add_subplot(111)
     norm = colors.Normalize(vmin=-np.pi, vmax=np.pi)
+    colours = cm.get_cmap(colourMap)
     axis.imshow(
         data,
-        cmap=colourMap,
+        cmap=colours,
         norm=norm,
         interpolation=None,
         extent=extent,
@@ -217,7 +273,7 @@ def plotComplexMatrixAbsOrPhase(
         norm = colors.Normalize(vmin=0, vmax=np.max(data))
         ticks = np.linspace(0, np.max(data), 5)
     cbar = fig.colorbar(
-        cm.ScalarMappable(norm=norm, cmap=colourMap),
+        cm.ScalarMappable(norm=norm, cmap=colours),
         ax=axis,
         shrink=0.8,
         pad=0.1,
@@ -226,32 +282,218 @@ def plotComplexMatrixAbsOrPhase(
     if phase:
         cbar.ax.set_yticklabels(["$-\\pi$", "$\\pi/2$", "0", "$\\pi/2$", "$\\pi$"])
 
+    # show and save
+    plt.tight_layout()
+    if filename:
+        plt.savefig(filename, bbox_inches="tight", dpi=100)
+    plt.show()
+    plt.close()
+
     return fig
 
 
-def plot_dynamics_plotly(exp, psi_init, seq, filename, goal=-1):
+def calculatePopulation(
+    exp: Experiment, psi_init: tf.Tensor, sequence: List[str]
+) -> np.array:
+    """
+    Calculates the time dependent population starting from a specific initial state.
+
+    Parameters
+    ----------
+    exp: Experiment
+        The experiment containing the model and propagators
+    psi_init: tf.Tensor
+        Initial state vector
+    sequence: List[str]
+        List of gate names that will be applied to the state
+
+    Returns
+    -------
+    np.array
+       two-dimensional array, first dimension: time, second dimension: population of the levels
+    """
+    # calculate the time dependent level population
     model = exp.pmap.model
     dUs = exp.partial_propagators
     psi_t = psi_init.numpy()
     pop_t = exp.populations(psi_t, model.lindbladian)
-    for gate in seq:
+    for gate in sequence:
         for du in dUs[gate]:
-            psi_t = np.matmul(du.numpy(), psi_t)
+            psi_t = np.matmul(du, psi_t)
             pops = exp.populations(psi_t, model.lindbladian)
             pop_t = np.append(pop_t, pops, axis=1)
+    return pop_t
 
-    ts = exp.ts
-    dt = ts[1] - ts[0]
+
+def plotPopulation(
+    exp: Experiment,
+    psi_init: tf.Tensor,
+    sequence: List[str],
+    labels: List[str] = None,
+    usePlotly=True,
+    vertical_lines=False,
+    filename: str = None,
+):
+    """
+    Plots time dependent populations. They need to be calculated with `runTimeEvolution` first.
+    Parameters
+    ----------
+    exp: Experiment
+        The experiment containing the model and propagators
+    psi_init: np.array
+        Initial state vector
+    sequence: List[str]
+        List of gate names that will be applied to the state
+    labels: List[str]
+        Optional list of names for the levels. If none, the default list from the experiment will be used.
+    usePlotly: bool
+        Whether to use Plotly or Matplotlib
+    vertical_lines: bool
+        If true, this add a dotted vertical line after each gate
+    filename: str
+        Optional name of the file to which the plot will be saved. If none,
+        it will only be shown.
+    """
+    # calculate the time dependent level population
+    model = exp.pmap.model
+    pop_t = calculatePopulation(exp, psi_init, sequence)
+
+    # timestamps
+    dt = exp.ts[1] - exp.ts[0]
     ts = np.linspace(0.0, dt * pop_t.shape[1], pop_t.shape[1])
 
-    legends = model.state_labels
-    fig = go.Figure()
-    for i in range(len(pop_t.T[0])):
-        fig.add_trace(
-            go.Scatter(x=ts / 1e-9, y=pop_t.T[:, i], mode="lines", name=str(legends[i]))
+    legend_labels = labels if labels else model.state_labels
+    labelX = "Time [ns]"
+    labelY = "Population"
+
+    # create the plot
+    if usePlotly:
+        fig = go.Figure()
+        for i in range(len(pop_t.T[0])):
+            fig.add_trace(
+                go.Scatter(
+                    x=ts / 1e-9,
+                    y=pop_t.T[:, i],
+                    mode="lines",
+                    name=str(legend_labels[i]),
+                )
+            )
+        fig.update_layout(xaxis_title=labelX, yaxis_title=labelY)
+    else:
+        fig, axs = plt.subplots(1, 1, figsize=[10, 5])
+        axs.plot(ts / 1e-9, pop_t.T)
+
+        # set plot properties
+        axs.tick_params(direction="in", left=True, right=True, top=False, bottom=True)
+        axs.set_xlabel(labelX)
+        axs.set_ylabel(labelY)
+        plt.legend(
+            legend_labels,
+            ncol=int(np.ceil(model.tot_dim / 15)),
+            bbox_to_anchor=(1.05, 1.0),
+            loc="upper left",
         )
+        plt.tight_layout()
 
-    fig.show()
-    # fig.write_html(filename+".html")
+    # plot vertical lines; TODO: does not work with Plotly yet!
+    if (not usePlotly) and vertical_lines and len(sequence) > 0:
+        gate_steps = [exp.partial_propagators[g].shape[0] for g in sequence]
+        for i in range(1, len(gate_steps)):
+            gate_steps[i] += gate_steps[i - 1]
+        gate_times = gate_steps * dt
+        if usePlotly:
+            for t in gate_times:
+                fig.add_vline(
+                    x=t / 1e-9, line_width=1, line_dash="dot", line_color="black"
+                )
+        else:
+            plt.vlines(
+                x=gate_times / 1e-9,
+                ymin=tf.reduce_min(pop_t),
+                ymax=tf.reduce_max(pop_t),
+                linestyles=":",
+                colors="black",
+            )
 
-    pass
+    # show and save
+    if usePlotly:
+        if filename:
+            fig.write_image(filename)
+        else:
+            fig.show()
+    else:
+        if filename:
+            plt.savefig(filename, bbox_inches="tight", dpi=100)
+            plt.close()
+        else:
+            plt.show()
+
+
+def plotSplittedPopulation(
+    exp: Experiment,
+    psi_init: tf.Tensor,
+    sequence: List[str],
+    vertical_lines=False,
+    filename: str = None,
+) -> None:
+    """
+    Plots time dependent populations for multiple qubits in separate plots.
+
+    Parameters
+    ----------
+    exp: Experiment
+        The experiment containing the model and propagators
+    psi_init: np.array
+        Initial state vector
+    sequence: List[str]
+        List of gate names that will be applied to the state
+    vertical_lines: bool
+        If true, this add a dotted vertical line after each gate
+    filename: str
+        Optional name of the file to which the plot will be saved. If none,
+        it will only be shown.
+    -------
+    """
+    # calculate the time dependent level population
+    model = exp.pmap.model
+    pop_t = calculatePopulation(exp, psi_init, sequence)
+    dims = [s.hilbert_dim for s in model.subsystems.values()]
+    splitted = getQubitsPopulation(pop_t, dims)
+
+    # timestamps
+    dt = exp.ts[1] - exp.ts[0]
+    ts = np.linspace(0.0, dt * pop_t.shape[1], pop_t.shape[1])
+
+    # positions of vertical lines
+    gate_steps = [exp.partial_propagators[g].shape[0] for g in sequence]
+    for i in range(1, len(gate_steps)):
+        gate_steps[i] += gate_steps[i - 1]
+    gate_times = gate_steps * dt
+
+    # create both subplots
+    fig, axs = plt.subplots(1, len(splitted), sharey="all")
+    for idx, ax in enumerate(axs):
+        ax.plot(ts / 1e-9, splitted[idx].T)
+        if vertical_lines:
+            ax.vlines(
+                gate_times / 1e-9,
+                tf.reduce_min(pop_t),
+                tf.reduce_max(pop_t),
+                linestyles=":",
+                colors="black",
+            )
+
+        # set plot properties
+        ax.tick_params(direction="in", left=True, right=True, top=False, bottom=True)
+        ax.set_xlabel("Time [ns]")
+        ax.set_ylabel("Population")
+        ax.legend([str(x) for x in np.arange(dims[idx])])
+
+    plt.tight_layout()
+
+    # show and save
+    if filename:
+        plt.savefig(filename, bbox_inches="tight", dpi=100)
+    else:
+        plt.show()
+    plt.close()
